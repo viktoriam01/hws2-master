0f992e83ed1df7d7ceb013634ba1987cdba85df4
viktoriam01 <aislingdar.com>
2022-12-07 21:13:02 +0800

    feat: css done

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -34,7 +34,7 @@ export const message0: MessageType = {
         name: 'Some Name',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'some textsome textsome textsome textsome textsome text', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -69,3 +69,4 @@ const HW1 = () => {
 }
 
 export default HW1
+
